f0bfd51c43392cf946363a77a9ad5a2212d8000d
Prashant Aggarwal <prashant.aggarwal.2@alight.com>
2022-03-23 11:37:59 +0530

    Added logs

--- src/main/java/com/pc/lambda/Handler.java
+++ src/main/java/com/pc/lambda/Handler.java
@@ -1,19 +1,14 @@
 package com.pc.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.pc.lambda.service.InvokeAPI;
 
 public class Handler implements RequestHandler<String, String> {
 
-	private static LambdaLogger logger = null;
-
 	public String handleRequest(String input, Context context) {
-		
-		System.out.println("Inside Lambda Handler....");
 
-		logger = context.getLogger();
+		System.out.println("==== Environment Variables ====");
 
 		// Read all of the environment variables
 		String clientId = System.getenv("appId");
@@ -21,35 +16,35 @@ public class Handler implements RequestHandler<String, String> {
 		String scope = System.getenv("scope");
 		String grantType = System.getenv("grantType");
 		String orchestratorBaseURL = System.getenv("orchestratorBaseURL");
-		
+
 		System.setProperty("https.proxyHost", System.getenv("https_proxyHost"));
 		System.setProperty("https.proxyPort", System.getenv("https_proxyPort"));
-		
-		logger.log("\nscope => " + scope);
-		logger.log("\ngrantType => " + grantType);
-		logger.log("\norchestratorBaseURL => " + orchestratorBaseURL);
+
+		System.out.println("scope => " + scope);
+		System.out.println("grantType => " + grantType);
+		System.out.println("orchestratorBaseURL => " + orchestratorBaseURL);
 		if (clientId != null && clientId.length() > 5) {
-			logger.log("\nclientId => " + clientId.substring(0, 4));
+			System.out.println("clientId => " + clientId.substring(0, 4));
 		} else {
-			logger.log("\nclientId not available");
+			System.out.println("clientId not available");
 		}
 		if (clientSecret != null && clientSecret.length() > 5) {
-			logger.log("\nclientSecret => " + clientSecret.substring(0, 4));
+			System.out.println("clientSecret => " + clientSecret.substring(0, 4));
 		} else {
-			logger.log("\nclientSecret not available");
+			System.out.println("clientSecret not available");
 		}
 
 		try {
 			InvokeAPI api = new InvokeAPI();
 			String releaseKey = api.callOrchestratorAPI(clientId, clientSecret, scope, grantType, orchestratorBaseURL);
-			return "Release Key: " + releaseKey;
+			return releaseKey;
 		} catch (Exception e) {
-			logger.log("\nException while performing file operations: " + e.getMessage() + " \nStack Trace: "
+			System.out.println("Exception while calling Orchestrator APIs: " + e.getMessage() + " \nStack Trace: "
 					+ e.getStackTrace());
 		}
 
-		logger.log("\nCompleted Lambda Handler !");
+		System.out.println("Completed Lambda Handler !");
 		return "";
 	}
-	
+
 }
